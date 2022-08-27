9c1cae18db4ceefd8becbbadefd08085d07d90ce
Troy Chaplin <troychaplin@Troys-MacBook-Pro.local>
2022-08-27 12:39:24 -0400

    Updated block config

---
+++ README.md
@@ -0,0 +1,5 @@
+## CU Blocks
+
+## Resources
+
+-   [List of Gutenberg blocks](https://rudrastyh.com/gutenberg/remove-default-blocks.html#block_slugs)
--- build/block-one/index.asset.php
+++ build/block-one/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd46d9f5b66618bb70f0b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f3e7f30ee4731cb150fc');
